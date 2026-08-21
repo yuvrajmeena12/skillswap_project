5c73ffdd2e236b95e716b8e1f3464858d87065fb
yuvrajmeena12 <yuviharsh1234@gmail.com>
2026-08-21 22:56:29 +0530

    Test Jenkins auto trigger

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+port { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
 import Navbar from './components/Navbar';
